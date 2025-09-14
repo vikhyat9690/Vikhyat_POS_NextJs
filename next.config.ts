f62a4d1068a5d90d46dd4986a1a4ff4c9c0606ba
Vikhyat Garg <vikhyatgarg9690@gmail.com>
2025-09-14 16:55:06 +0530

    Fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8888",
+        pathname: "/mage_Blog/**"
+      }
+    ]
+  },
   webpack: (config) => {
     config.module.rules.push({
         test: /\.(graphql|gql)$/,
